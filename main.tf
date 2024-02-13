277e774a9b453124971fc0f75685e97e9d2633cd
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2024-02-12 22:35:54 -0500

    [ADD]: Complete files

--- main.tf
+++ main.tf
@@ -13,3 +13,29 @@ provider "google" {
   region      = var.region
   zone        = var.zone
 }
+
+resource "google_compute_network" "vpc_network" {
+  name                            = var.vpc_name
+  auto_create_subnetworks         = var.vpc_auto_create_subnetworks_false
+  routing_mode                    = var.vpc_routing_mode_regional
+  delete_default_routes_on_create = var.vpc_delete_default_routes_on_create_true
+}
+
+resource "google_compute_subnetwork" "webapp" {
+  name          = var.public_subnet_name
+  ip_cidr_range = var.webapp_subnet_cidr
+  network       = google_compute_network.vpc_network.name
+}
+
+resource "google_compute_subnetwork" "db" {
+  name          = var.private_subnet_name
+  ip_cidr_range = var.db_subnet_cidr
+  network       = google_compute_network.vpc_network.name
+}
+
+resource "google_compute_route" "webapp" {
+  name             = var.route_name
+  dest_range       = var.webbapp_internet_gateway_route_dest
+  network          = google_compute_network.vpc_network.name
+  next_hop_gateway = var.webbapp_internet_gateway_route_next_hop_gateway
+}
--- variables.tf
+++ variables.tf
@@ -20,12 +20,33 @@ variable "vpc_name" {
   description = "Name of the VPC network"
   default     = "terraform-network"
 }
+variable "vpc_auto_create_subnetworks_false" {
+  description = "assigns false value to the auto_create_subnetworks for vpc"
+  default     = false
+}
+
+variable "vpc_routing_mode_regional" {
+  description = "assigns regional to the vpc_routing_mode"
+  default     = "REGIONAL"
+}
+
+variable "vpc_delete_default_routes_on_create_true" {
+  description = "assigns true value to the vpc_delete_default_routes"
+  default     = true
+}
+variable "public_subnet_name" {
+  description = "name of the subnet of the web app"
+  default     = "webapp"
+}
 
 variable "webapp_subnet_cidr" {
   description = "CIDR range for the webapp subnet"
   default     = "10.0.2.0/24"
 }
-
+variable "private_subnet_name" {
+  description = "name of the subnet of the db"
+  default     = "db"
+}
 variable "db_subnet_cidr" {
   description = "CIDR range for the db subnet"
   default     = "10.0.1.0/24"
@@ -34,4 +55,15 @@ variable "db_subnet_cidr" {
 variable "route_name" {
   description = "Name of the route for the webapp subnet"
   default     = "webapp-internet-gateway-route"
+}
+
+variable "webbapp_internet_gateway_route_dest" {
+  description = "destination for the route webapp-internet-gateway-route"
+  default     = "0.0.0.0/0"
+
+}
+variable "webbapp_internet_gateway_route_next_hop_gateway" {
+  description = "next hop gateway for the route webapp-internet-gateway-route"
+  default     = "default-internet-gateway"
+
 }
