43b88bc0647140850189464a2967e788f2bea6b8
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2024-02-21 19:11:18 -0500

    update Variables

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "vpc_network" {
-  count                           = var.num_vpcs
+  # count                           = var.num_vpcs
   name                            = "${var.vpc_name}-${uuid()}"
   auto_create_subnetworks         = var.vpc_auto_create_subnetworks_false
   routing_mode                    = var.vpc_routing_mode_regional
@@ -7,23 +7,23 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_subnetwork" "webapp" {
-  count         = var.num_vpcs
-  name          = count.index == 0 ? var.public_subnet_name : "${var.public_subnet_name}-${uuid()}"
-  ip_cidr_range = cidrsubnet(var.vpc_cidr_range, 8, count.index + 1)
-  network       = google_compute_network.vpc_network[count.index].name
+  # count         = var.num_vpcs
+  name          = var.public_subnet_name 
+  ip_cidr_range = webapp_cidr_range
+  network       = google_compute_network.vpc_network.name
 }
 
 resource "google_compute_subnetwork" "db" {
-  count         = var.num_vpcs
-  name          = count.index == 0 ? var.private_subnet_name : "${var.private_subnet_name}-${uuid()}"
-  ip_cidr_range = cidrsubnet(var.vpc_cidr_range, 8, count.index + 128)
-  network       = google_compute_network.vpc_network[count.index].name
+  # count         = var.num_vpcs
+  name          = var.private_subnet_name
+  ip_cidr_range = db_cidr_range
+  network       = google_compute_network.vpc_network.name
 }
 
 resource "google_compute_route" "internet_gateway_route" {
-  count            = var.num_vpcs
+  # count            = var.num_vpcs
   name             = "${var.route_name}-${uuid()}"
   dest_range       = var.webapp_internet_gateway_route_dest
-  network          = google_compute_network.vpc_network[count.index].name
+  network          = google_compute_network.vpc_network.name
   next_hop_gateway = var.webapp_internet_gateway_route_next_hop_gateway
 }
