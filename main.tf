12df51970271ce1ef881d4f193bc3994ea0684ec
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2024-02-27 17:18:08 -0500

    [ADD]: Assignement 4

--- main.tf
+++ main.tf
@@ -11,7 +11,9 @@ resource "google_compute_subnetwork" "webapp" {
   # name          = count.index == 0 ? var.public_subnet_name : "${var.public_subnet_name}-${uuid()}"
   name          = var.public_subnet_name
   ip_cidr_range = var.webapp_cidr_range
+  region = var.region
   network       = google_compute_network.vpc_network.name
+  private_ip_google_access =var.subnet_webapp_private_ip_google_access
 }
 
 resource "google_compute_subnetwork" "db" {
@@ -19,6 +21,7 @@ resource "google_compute_subnetwork" "db" {
   # name          = count.index == 0 ? var.private_subnet_name : "${var.private_subnet_name}-${uuid()}"
   name          = var.private_subnet_name
   ip_cidr_range = var.db_cidr_range
+  region = var.region
   network       = google_compute_network.vpc_network.name
 }
 
@@ -57,6 +60,16 @@ resource "google_compute_firewall" "deny_rule" {
   source_ranges = var.firewall_src_range
 }
 
+data "template_file" "startup_script"{
+  template = "${file("scripts/startup.sh")}"
+  vars = {
+    DB_INTERNAL_IP_ADDRESS = "${google_sql_database_instance.main_primary.private_ip_address}",
+    DB_USERNAME =var.google_sql_user_webapp_name,
+    DB_PASSWORD ="${random_password.password.result}",
+    DB_NAME = var.google_sql_database_webapp_name,
+    location =var.properties_location
+  }
+}
 resource "google_compute_instance" "custom_vm_instance" {
   name         = var.vm_instance_name
   zone         = var.vm_instance_zone
@@ -78,4 +91,59 @@ resource "google_compute_instance" "custom_vm_instance" {
       // Assigns a public IP address
     }
   }
+  metadata_startup_script = "${data.template_file.startup_script.rendered}"
+}
+
+# Create a private address range for our network
+resource "google_compute_global_address" "private_ip_block" {
+  name         = var.private_ip_block_name
+  purpose      = var.private_ip_block_purpose
+  address_type = var.private_ip_block_address_type
+  ip_version   = var.private_ip_block_ip_version
+  prefix_length = var.private_ip_block_prefix_length
+  network       = google_compute_network.vpc_network.self_link
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc_network.self_link
+  service                 = var.private_vpc_connection_service
+  reserved_peering_ranges = [google_compute_global_address.private_ip_block.name]
+}
+
+resource "google_sql_database" "webapp" {
+  name     = var.google_sql_database_webapp_name
+  instance = google_sql_database_instance.main_primary.name
+}
+resource "google_sql_database_instance" "main_primary" {
+  name             = var.sql_instance_name
+  database_version = var.database_version
+  region = var.region
+  depends_on       = [google_service_networking_connection.private_vpc_connection]
+  settings {
+    tier              = var.database_instance_tier
+    availability_type = var.database_instance_availability_type
+    disk_size         = var.database_instance_disk_size
+    disk_type = var.database_instance_disk_type
+    ip_configuration {
+      ipv4_enabled    = var.database_instance_IPV4_enabled
+      private_network = google_compute_network.vpc_network.self_link
+    }
+    backup_configuration {
+      enabled            = var.backup_configuration_enabled
+      binary_log_enabled = var.backup_configuration_binary_log_enabled
+    }
+  }
+  deletion_protection  = var.database_instance_deletion_protection
+}
+
+resource "random_password" "password" {
+  length           = var.password_restriction_length
+  special          = var.password_restriction_special_characters_allowed
+  override_special = var.password_restriction_override_special
+}
+
+resource "google_sql_user" "db_user" {
+  name     = var.google_sql_user_webapp_name
+  instance = google_sql_database_instance.main_primary.name
+  password = random_password.password.result
 }
---
+++ scripts/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "spring.datasource.driver-class-name=com.mysql.jdbc.Driver" >> application.properties
+echo "spring.datasource.url=jdbc:mysql://${DB_INTERNAL_IP_ADDRESS}/${DB_NAME}?createDatabaseIfNotExist=true&useUnicode=true&characterEncoding=utf8" >> application.properties
+echo "spring.datasource.username=${DB_USERNAME}" >> application.properties
+echo "spring.datasource.password=${DB_PASSWORD}" >> application.properties
+echo "spring.jpa.properties.hibernate.show_sql=true" >> application.properties
+echo "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQLDialect" >> application.properties
+echo "spring.jpa.hibernate.ddl-auto=update" >> application.properties
+echo "spring.datasource.hikari.connection-timeout=2000" >> application.properties
+echo "logging.level.org.springframework.validation=DEBUG" >> application.properties
+echo "spring.jackson.deserialization.fail-on-unknown-properties=true" >> application.properties
+
+sudo mv application.properties ${location}
