f8b8e1f558b76132926c164c0a55900d91be792c
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2024-03-27 12:38:48 -0400

    [ADD]: add assignment

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -10,6 +10,8 @@ echo "spring.jpa.hibernate.ddl-auto=update" >> application.properties
 echo "spring.datasource.hikari.connection-timeout=2000" >> application.properties
 echo "logging.level.org.springframework.validation=DEBUG" >> application.properties
 echo "spring.jackson.deserialization.fail-on-unknown-properties=true" >> application.properties
+echo "pubsub.projectId=${PROJECT_ID}" >> application.properties
+echo "pubsub.topicId=${TOPIC_ID}" >> application.properties
 ls -al >>debug.txt
 sudo chown csye6225:csye6225 application.properties
 sudo mv application.properties ${location}
